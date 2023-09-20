850e7688e13d4ca76cd38a52d4d10eaad1057a23
Sami Sardarzai <cleversam07@gmail.com>
2023-09-20 01:09:28 -0700

    add new changes

--- bg-changer3/src/App.jsx
+++ bg-changer3/src/App.jsx
@@ -6,14 +6,14 @@ function App() {
 
   return (
     <>
-      <div className='w-full h-screen duration-200 ' 
+      <div className=' p-10 m-4 mb-4 rounded-md min-w-fit h-screen duration-200 ' 
        style={ {backgroundColor : color}}
       >
         <div className='fixed flex flex-wrap justify-center top-3 inset-x-0 px-2'>
           <div className=' flex flex-wrap justify-center gap-3 shadow-lg py-2 rounded-3xl bg-slate-400 px-3  '>
             <button  onClick={()=>setColor("black")}className=' outline-none px-4 py-1 rounded-full text-white shadow-lg '
              style={{backgroundColor:" black"}}
-            >Black</button>
+            >Blacka</button>
             <button  onClick={()=>setColor("red")}className=' outline-none px-4 py-1 rounded-full text-white shadow-lg '
              style={{backgroundColor:"red"}}
             >Red</button>
